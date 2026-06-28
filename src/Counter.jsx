07af3abfc04299c036b354aeec2fbca31b3b6e01
asm-saim <saim777m@gmail.com>
2026-06-28 23:32:58 +0600

    useSate hook implemented

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,22 @@
- export default function Counter() {
-    const counterStyle={
-        border:"2px solid yellow",
-        padding:"20px"
+import { useState } from "react"
+
+export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    const handleCount = () => {
+        const newCount = count + 1;
+        setCount(newCount)
+    }
+
+    const counterStyle = {
+        border: "2px solid yellow",
+        padding: "20px"
     }
     return (
         <div style={counterStyle}>
             <h3>Introducing Counter, by using state Change</h3>
-            <p>Count: </p>
-            <button>Add</button>
+            <p>Count: {count} </p>
+            <button onClick={handleCount} >Add</button>
         </div>
     )
 }
