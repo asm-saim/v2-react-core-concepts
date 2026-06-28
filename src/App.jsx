a9db3a6c074e20947fb7510f2a27538ed72690ff
asm-saim <saim777m@gmail.com>
2026-06-28 21:00:41 +0600

    event handler in react inicialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 
 function App() {
+  function handleClick() {
+    alert("I am clicked.")
+  }
 
   return (
     <>
-      <h1>Get started</h1>
+      <div id='center'>
+        <h1>Get started</h1>
+        {/* <button onclick="handleClick()">Click</button>-> in js we use this */}
+        <button onClick={handleClick}>Click</button>
+      </div>
     </>
   )
 }
