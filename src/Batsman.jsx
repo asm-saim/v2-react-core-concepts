475604651332e9e0d09358052d167e4ace45df0f
asm-saim <saim777m@gmail.com>
2026-06-29 05:47:47 +0600

    useState is a Hook used to store and update batsman score

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -8,11 +8,23 @@ const handleSingle=()=>{
     setScore(scoreSingle)
 }
 
+const handleFour=()=>{
+    const scoreFour=score+4;
+    setScore(scoreFour);
+}
+
+const handleSix=()=>{
+    const scoreSix=score+6;
+    setScore(scoreSix)
+}
+
     return (
         <div>
             <h2>Batsman's score Counter</h2>
             <h2>Score: {score}</h2>
             <button onClick={handleSingle}>Add Single</button>
+            <button onClick={handleFour}>Add Four</button>
+            <button onClick={handleSix}>Add Six</button>
         </div>
     )
 }
