4706b0e0c2f693c39f91ffce6190a9d719230d1f
asm-saim <saim777m@gmail.com>
2026-06-29 06:14:28 +0600

    useState is a Hook used in nested way

--- src/Batsman.jsx
+++ src/Batsman.jsx
@@ -2,26 +2,32 @@ import { useState } from "react"
 
 export default function Batsman() {
     const [score, setScore] = useState(0)
+    const [six, setSix] = useState(0)
 
-const handleSingle=()=>{
-    const scoreSingle=score+1;
-    setScore(scoreSingle)
-}
+    const handleSingle = () => {
+        const scoreSingle = score + 1;
+        setScore(scoreSingle)
+    }
 
-const handleFour=()=>{
-    const scoreFour=score+4;
-    setScore(scoreFour);
-}
+    const handleFour = () => {
+        const scoreFour = score + 4;
+        setScore(scoreFour);
+    }
 
-const handleSix=()=>{
-    const scoreSix=score+6;
-    setScore(scoreSix)
-}
+    const handleSix = () => {
+        const scoreSix = score + 6;
+        setScore(scoreSix)
+
+        const sixCounter = six + 1;
+        setSix(sixCounter)
+
+    }
 
     return (
         <div>
             <h2>Batsman's score Counter</h2>
-            <h2>Score: {score}</h2>
+            <h2>Score: {score}</h2> 
+            <h2>Total Six:{six}</h2>
             <button onClick={handleSingle}>Add Single</button>
             <button onClick={handleFour}>Add Four</button>
             <button onClick={handleSix}>Add Six</button>
