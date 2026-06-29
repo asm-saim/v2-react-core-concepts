794c9b00759b4ceb948e444fa73cf26317ff8373
asm-saim <saim777m@gmail.com>
2026-06-29 22:46:51 +0600

    toggle  show and hide using useState - true, !true

--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 
+// only hide:
 // export default function Toggle() {
 //     const [value, setValue] = useState(true)
 
@@ -11,13 +12,15 @@ import { useState } from "react"
 //         </div>
 //     )
 // }
+
+// toggle: show/hide
 export default function Toggle() {
     const [value, setValue] = useState(true)
 
     return (
         <div className="universal">
             {value && <h2>Hello, React Learners!</h2>}
-            <button onClick={()=>setValue(false)}>Hide</button>
+            <button onClick={() => setValue(!value)}>{value ? "Hide" : "Show"}</button>
 
         </div>
     )
