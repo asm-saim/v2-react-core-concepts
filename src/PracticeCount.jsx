2ddcbc95cf34816d15484ffafb70ea4115a0b73d
asm-saim <saim777m@gmail.com>
2026-06-29 21:07:40 +0600

    counter- recaped

--- src/PracticeCount.jsx
+++ src/PracticeCount.jsx
@@ -3,14 +3,21 @@ import { useState } from "react"
 export default function PracticeCount() {
     const [count, setCount] = useState(0)
 
-    const value = () => {
+    const countPlus = () => {
         const newCount = count + 1;
+        setCount(newCount)
+    }
+
+    const countMinus = () => {
+        const newCount = count - 1;
         setCount(newCount)
     }
     return (
         <div className="universal">
             <h2>Count:{count}</h2>
-            <button onClick={value}>Count +</button> 
+            <button onClick={countPlus}>Count +</button>
+            <br />
+            <button onClick={countMinus}>Count -</button>
 
         </div>
     )
